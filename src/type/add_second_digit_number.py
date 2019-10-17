657cc8084a296104e902007604a6d3fb60992a2c
U-MATTYA-PC\mattya <watanabemsaki0527@gmail.com>
2019-10-17 18:16:03 +0900

    fix type/add_second_digit_number

--- src/type/add_second_digit_number.py
+++ src/type/add_second_digit_number.py
@@ -1,2 +1,7 @@
 def add_second_digit_number(num1, num2):
-    return num1 + num2
+    num1=str(num1)
+    num2=str(num2)
+    list_num1=list(num1)
+    list_num2=list(num2)
+
+    return (int)(list_num1[-2])+(int)(list_num2[-2])
--- src/type/calc_number_of_digit.py
+++ src/type/calc_number_of_digit.py
@@ -1,8 +1,3 @@
 def calc_number_of_digit(num):
-    cnt = 0
-    while num > 0:
-        t = num%10
-        for j in range(10):
-            if t == j : cnt += 1
-        num = num//10
-    return cnt
+
+    return len(range(num))
