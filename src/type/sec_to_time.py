9455bd81c4b8ef163fb03b43e4ea54a4e11e1f79
渋谷 <shibuya@wlan-napt-001.u-aizu.ac.jp>
2019-10-17 16:34:21 +0900

    fix type/sec_to_time

--- src/type/sec_to_time.py
+++ src/type/sec_to_time.py
@@ -1,3 +1,6 @@
 # return hh:mm:ss
 def sec_to_time(sec):
-    return sec/60
+    h = str(sec // 3600)
+    m = str((sec-int(h)*3600) // 60)
+    s = str((sec-int(h)*3600) % 60)
+    return (h.zfill(2) + ":" + m.zfill(2) + ":" + s.zfill(2))
