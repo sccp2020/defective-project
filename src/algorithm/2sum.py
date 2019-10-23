1c35587a2f4095e86098bc75aead24553a5327c4
NoahOrberg <noah.orberg.9999@gmail.com>
2019-10-23 23:48:13 +0900

    add problems and tests

---
+++ src/algorithm/2sum.py
@@ -0,0 +1,5 @@
+# 2sum
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# e.g, nums = [2, 7, 11, 15], target = 9, then return [0, 1] (or [1, 0])
+def 2sum(nums, target):
+    return [1,0]
---
+++ tests/type/test_2sum.py
@@ -0,0 +1,10 @@
+import unittest
+from src.type.add_second_digit_number import add_second_digit_number
+
+
+class TestTwoSum(unittest.TestCase):
+    def test_two_sum(self):
+        actual = 
+        expected = 5
+        self.assertEqual(actual, expected)
+
