8bf79fd71ca2738c47cf7d36a4c4b1386d940a24
schktjm <si2.o.mjt@gmail.com>
2019-10-24 16:39:41 +0900

    add round problem

---
+++ tests/type/test_round_down_diff.py
@@ -0,0 +1,14 @@
+import unittest
+from src.type.round_down_diff import round_down_diff
+
+
+class TestRoundDownDiff(unittest.TestCase):
+    def test_round_time_diff1(self):
+        actual = round_down_diff(32.8, 22.3)
+        expected = 10
+        self.assertEqual(actual, expected)
+
+    def test_round_time_diff2(self):
+        actual = round_down_diff(12.4, 9.9)
+        expected = 3
+        self.assertEqual(actual, expected)
