b9c660d328e4834b5f840bd91f0c60dc376b9467
schktjm <si2.o.mjt@gmail.com>
2019-10-24 16:49:24 +0900

    fix

--- tests/type/test_round_down_diff.py
+++ tests/type/test_round_down_diff.py
@@ -3,12 +3,12 @@ from src.type.round_down_diff import round_down_diff
 
 
 class TestRoundDownDiff(unittest.TestCase):
-    def test_round_time_diff1(self):
+    def test_round_down_diff1(self):
         actual = round_down_diff(32.8, 22.3)
         expected = 10
         self.assertEqual(actual, expected)
 
-    def test_round_time_diff2(self):
+    def test_round_down_diff2(self):
         actual = round_down_diff(12.4, 9.9)
         expected = 3
         self.assertEqual(actual, expected)
