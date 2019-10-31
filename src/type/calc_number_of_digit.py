b994776553115e1ac6451f28d7b3fc0b2c527975
Kuri174 <oxyshower@gmail.com>
2019-10-31 22:19:32 +0900

    fix type/calc_number_of_digit

--- src/type/calc_number_of_digit.py
+++ src/type/calc_number_of_digit.py
@@ -1,8 +1,2 @@
 def calc_number_of_digit(num):
-    cnt = 0
-    while num > 0:
-        t = num%10
-        for j in range(10):
-            if t == j : cnt += 1
-        num = num//10
-    return cnt
+    return len(str(num))
