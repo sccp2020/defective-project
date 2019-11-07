0344d0efeabf77c4b00784351076f4299ef2556a
ShujiOba <shuji365630@gmail.com>
2019-11-07 18:32:39 +0900

    Added determination of leap year

---
+++ src/algorithm/leap_year_normal.py
@@ -0,0 +1,8 @@
+# Write code to determine leap year
+# 1. Years in which the year is divisible by 4 are (in principle) leap years.
+# 2. However, the year in which the year is divisible by 100 is (in principle) a normal year.
+# 3. However, the year in which the year is divisible by 400 is always a leap year.
+
+def is_leap_year(year):
+    return False
+
