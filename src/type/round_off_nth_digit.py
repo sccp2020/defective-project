2af56119188bad7fa34f8e77a896bd5eb743005d
mattya527 <watanabemasaki0527@gmail.com>
2019-11-14 18:22:45 +0900

    fix type-round_off_nth_digit

--- src/type/round_off_nth_digit.py
+++ src/type/round_off_nth_digit.py
@@ -1,5 +1,10 @@
 def round_off_nth_digit(num, digit):
-    return num - digit
+    num2 = num * 10**digit
+    num2 = int(num2)
+    l = list(str(num2))
+    if int(l[-1]) >= 5:
+         num2 = num2+1
+    return num2/10**digit
 
 # hint : 小数点n桁目を四捨五入する関数
 # e.g. : (123.456, 2) -> 123.46
