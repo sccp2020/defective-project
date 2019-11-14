00e918a3dd6b0a58e8267808d1f5d4fa9c1741fe
s1260206-maruyama <s1260206@u-aizu.ac.jp>
2019-11-14 18:23:13 +0900

    fix algorithm/test_isort_hard

--- src/algorithm/isort_hard.py
+++ src/algorithm/isort_hard.py
@@ -3,7 +3,16 @@
 
 # insert(3, [1, 2, 4, 5]) -> [1, 2, 3, 4, 5] 
 def insert(x, arr):
-    return []
+    if x<=0:
+        return(arr)
+    for i in range(len(arr)-x):
+        if arr[i]>arr[len(arr)-x]:
+            a=arr[len(arr)-x]
+            del arr[len(arr)-x]
+            arr.insert(i,a)
+            break
+
+    return insert(x-1,arr)
 
 def isort(arr):
-    return []
+    return insert(len(arr),arr)
