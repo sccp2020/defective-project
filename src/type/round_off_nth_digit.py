1d4d1741ddcc9a297128095b0703593be2421e5e
aizu-Eiki-Mori <s1260208@u-aizu.ac.jp>
2019-11-14 18:23:59 +0900

    fix type/round_of_nth_digit

--- src/type/round_off_nth_digit.py
+++ src/type/round_off_nth_digit.py
@@ -1,4 +1,16 @@
 def round_off_nth_digit(num, digit):
-    return round(num,digit)
+    count = digit
+    x = num + 0.0
+    while(count == 0):
+        x = x * 10
+        count -= 1
+    ans = int(x + 0.5)
+    count = digit
+    x = ans + 0.0
+    while(count == 0):
+        x = x / 10
+        count -= 1
+    return x
+
 # hint : 小数点n桁目を四捨五入する関数
 # e.g. : (123.456, 2) -> 123.46
