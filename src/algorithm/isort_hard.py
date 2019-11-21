5f5b3817afb26dbd10d73ae026dc2017565dbd97
s1260206-maruyama <s1260206@u-aizu.ac.jp>
2019-11-21 20:30:34 +0900

    fix algorithm/isort_hard

--- src/algorithm/isort_hard.py
+++ src/algorithm/isort_hard.py
@@ -1,17 +1,18 @@
 # Implement insertion sort.
 # You can not use loop
 
-# insert(3, [1, 2, 4, 5]) -> [1, 2, 3, 4, 5] 
-def insert(x, arr):
-    if x<=0:
-        return(arr)
-    for i in range(len(arr)-x):
-        if arr[i]>arr[len(arr)-x]:
-            a=arr[len(arr)-x]
-            del arr[len(arr)-x]
-            arr.insert(i,a)
+# insert(3, [1, 2, 4, 5]) -> [1, 2, 3, 4, 5]
 
-    return insert(x-1,arr)
+def insert(x,arr):
+    for i in range(len(arr)):
+        if (x <= arr[i]) :
+            arr.insert(i,x)
+            return arr
+    arr.insert(len(arr),x)
+    return arr    
 
 def isort(arr):
-    return insert(len(arr),arr)
+    toarr=[]
+    for i in range(len(arr)):
+        toarr=insert(arr[i],toarr)
+    return toarr
