1721d3a5d8adaedbf3cb510eee020dd8f294eb5b
Kuri174 <oxyshower@gmail.com>
2019-12-19 17:48:28 +0900

    fix algorithm-msort_hard

--- src/algorithm/msort_hard.py
+++ src/algorithm/msort_hard.py
@@ -18,17 +18,16 @@ def merge(arr1, arr2) :
             arr.append(arr1.pop(0))
         else :
             arr.append(arr2.pop(0))
-    if len(arr1) != 0 :
+    if len(arr1) != 0 : 
         arr.extend(arr1)
-    elif len(arr2) != 0 :
+    else :
         arr.extend(arr2)
     return arr
 
-def msort(arr, left = -1, right = -1) :
-    if left == -1 : left, right = 0, len(arr)
-    if left+1 < right :
-        mid = (left + right) // 2
-        msort(arr, left, mid)
-        msort(arr, mid, right)
-        arr[left : right] = merge(arr[left : mid], arr[mid : right])
+def msort(arr) :
+    if len(arr) <= 1 : return arr;
+    mid = len(arr) // 2
+    arr[0 : mid] = msort(arr[0 : mid])
+    arr[mid : len(arr)] = msort(arr[mid : len(arr)])
+    arr[0 : len(arr)] = merge(arr[0 : mid], arr[mid : len(arr)])
     return arr
