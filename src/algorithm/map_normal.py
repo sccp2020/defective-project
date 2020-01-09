75f709cca359b04aa1280eb92d799b5bf549d09c
schktjm <si2.o.mjt@gmail.com>
2020-01-09 18:14:43 +0900

    feature: map_normal

---
+++ src/algorithm/map_normal.py
@@ -0,0 +1,7 @@
+# Recursively combines all elements of the specified array
+# by applying the operation specified in the callback.
+# e.g.
+# map_normal([1,2,3], x*2 ) => [2, 4, 6]
+
+def map_normal(arr, callback):
+    return arr
