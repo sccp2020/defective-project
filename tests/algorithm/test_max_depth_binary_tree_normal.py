3eea546f4e4e395eb6782c8b829d4819545176e0
schktjm <si2.o.mjt@gmail.com>
2020-01-09 18:54:11 +0900

    これ通れば大丈夫そう

--- tests/algorithm/test_max_depth_binary_tree_normal.py
+++ tests/algorithm/test_max_depth_binary_tree_normal.py
@@ -16,3 +16,21 @@ class TestMaxDepthBinaryTree(unittest.TestCase):
         expected = 5
         actual = max_depth_binary_tree([1, 2, 3, 4, 5, 6, None, 8, 9, 10, 11, 12, 13, 14])
         self.assertEqual(actual, expected)
+
+    def test_max_depth_binary_tree_cond_4(self):
+        tree = [1, None, 4, None, None, None, 9, None, None, None, None, None, None, 2, None]
+        expected = 4
+        actual = max_depth_binary_tree(tree)
+        self.assertEqual(actual, expected)
+
+    def test_max_depth_binary_tree_cond_5(self):
+        tree = []
+        expected = 0
+        actual = max_depth_binary_tree(tree)
+        self.assertEqual(actual, expected)
+
+    def test_max_depth_binary_tree_cond_6(self):
+        tree = [1]
+        expected = 1
+        actual = max_depth_binary_tree(tree)
+        self.assertEqual(actual, expected)
